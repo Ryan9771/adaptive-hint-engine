9156cda02b12362aed2bb746d765e12f6724987d
Ryan <rkp21@ic.ac.uk>
2025-02-18 02:19:35 +0000

    FEAT/WIP: Implemented db

--- backend/main.py
+++ backend/main.py
@@ -3,21 +3,47 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from example_exercises.kotlin.exercise_1 import exercise_1
 from agent_trials.multi_agent import MultiAgent
+from setup_db import db_session, ExerciseEntry, add_exercise, get_exercise, add_feature_attempt, get_feature_attempts
+
+# Kotlin Exercise 1
+
+student_code = exercise_1['student_code']
+question = exercise_1['question']
+skel_code = exercise_1['skel_code']
+exercise_1_key = "kotlin_exercise_1"
+
+exercise = ExerciseEntry(exercise_key=exercise_1_key,
+                         exercise_text=question, skel_code=skel_code)
+
+three_sample_feature_attempts = [
+    ["syntax_error", "missing_colon", "runtime_error", "undeclared_variable"],
+    ["incorrect_logic", "off_by_one_error",
+        "hardcoded_values", "missing_modulo_check"],
+    ["inefficient_loop", "unnecessary_conditionals",
+        "repetitive_code", "poor_variable_naming"]
+]
+
+map(lambda xs: add_feature_attempt(exercise_key=exercise_1_key,
+    feature_attempt=xs), three_sample_feature_attempts)
 
 
-class Features:
-    features: list[str]
+print(get_feature_attempts(exercise_key=exercise_1_key, last_n=3))
+
+# == Agent Run Attempt ==
+
+# class Features:
+#     features: list[str]
 
 
-llm = ChatOpenAI(model="gpt-4o")
+# llm = ChatOpenAI(model="gpt-4o")
 
-if __name__ == "__main__":
-    print("== Initialising Agent ==")
-    agent = MultiAgent()
+# if __name__ == "__main__":
+#     print("== Initialising Agent ==")
+#     agent = MultiAgent()
 
-    student_code = exercise_1['student_code']
-    question = exercise_1['question']
-    skel_code = exercise_1['skel_code']
+#     student_code = exercise_1['student_code']
+#     question = exercise_1['question']
+#     skel_code = exercise_1['skel_code']
 
-    agent.run({"skel_code": skel_code, "question": question,
-              "student_code": student_code})
+#     agent.run({"skel_code": skel_code, "question": question,
+#               "student_code": student_code})
--- backend/setup_db.py
+++ backend/setup_db.py
@@ -1,27 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, String, Text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("sqlite:///questions.db", echo=True)
+engine = create_engine("sqlite:///exercise.db", echo=True)
 db_session = scoped_session(sessionmaker(
     autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
 
-class QuestionEntry(Base):
-    __tablename__ = "questions"
+class ExerciseEntry(Base):
+    __tablename__ = "exercises"
 
-    id = Column(Integer, primary_key=True)
-    question_text = Column(String(255), nullable=False)
-    feature_attempts = Column(JSON, default={})
+    exercise_key = Column(String(255), primary_key=True)
+    exercise_text = Column(Text, nullable=False)
+    skel_code = Column(Text, nullable=False)
 
-    def __init__(self, question_text):
+    # The list of features extracted from student code attempts.
+    feature_attempts = Column(JSON, default=[])
+
+    def __init__(self, exercise_key, exercise_text, skel_code):
         """Initialise a question with empty attempts"""
-        self.question_text = question_text
-        self.feature_attempts = {}
+        self.exercise_key = exercise_key
+        self.exercise_text = exercise_text
+        self.skel_code = skel_code
+        self.feature_attempts = []
 
     def add_feature_attempt(self, feature_list):
         """Adds a feature attempt with an incrementing index"""
@@ -31,4 +36,46 @@ class QuestionEntry(Base):
         db_session.commit()
 
 
+def add_exercise(exercise_key, exercise_text, skel_code):
+    """
+    Adds an exercise to the database with:
+        exercise_key: primary key
+        exercise_text: The description / question
+        skel_code: The skeleton code
+    """
+    exercise = ExerciseEntry(exercise_key, exercise_text, skel_code)
+    db_session.add(exercise)
+    db_session.commit()
+
+
+def get_exercise(exercise_key):
+    """Gets an exercise using the exercise key"""
+    exercise = db_session.query(ExerciseEntry).filter_by(
+        exercise_key=exercise_key).first()
+    if exercise:
+        return exercise
+
+    return None
+
+
+def add_feature_attempt(exercise_key, feature_attempt: list[str]):
+    """Adds a list of features of the latest student code attempt"""
+    exercise = get_exercise(exercise_key=exercise_key)
+
+    if exercise:
+        exercise.add_feature_attempt(feature_attempt)
+
+
+def get_feature_attempts(exercise_key, last_n):
+    """Gets the latest n list of features"""
+    exercise = get_exercise(exercise_key=exercise_key)
+
+    if exercise:
+        return exercise.feature_attempts[-last_n:]
+
+    return None
+
+
+# Create the database
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)

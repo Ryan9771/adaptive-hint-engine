7abc18b0f166bcd72af3618be5c909ae0b213441
Ryan <rkp21@ic.ac.uk>
2025-05-04 22:00:17 +0100

    FEAT: modified db schema to fit new agents

--- backend/setup_db.py
+++ backend/setup_db.py
@@ -3,9 +3,10 @@ from sqlalchemy import create_engine, Column, String, Text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.mutable import MutableDict
+from sqlalchemy.ext.mutable import MutableDict, MutableList
+from typing import List
 
-engine = create_engine("sqlite:///exercise.db", echo=True)
+engine = create_engine("sqlite:///new-exercise.db", echo=True)
 db_session = scoped_session(sessionmaker(
     autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
@@ -18,21 +19,25 @@ class ExerciseEntry(Base):
     exercise_key = Column(String(255), primary_key=True)
     exercise_text = Column(Text, nullable=False)
     skel_code = Column(Text, nullable=False)
-
-    # The list of features extracted from student code attempts.
-    feature_attempts = Column(MutableDict.as_mutable(JSON), default={})
+    required_concepts = Column(MutableList.as_mutable(JSON), default=list)
+    feature_attempts = Column(MutableList.as_mutable(JSON), default=list)
 
     def __init__(self, exercise_key, exercise_text, skel_code):
         """Initialise a question with empty attempts"""
         self.exercise_key = exercise_key
         self.exercise_text = exercise_text
         self.skel_code = skel_code
-        self.feature_attempts = {}
+        self.required_concepts = []
+        self.feature_attempts = []
+
+    def set_required_concepts(self, required_concepts: List[str]):
+        self.required_concepts = required_concepts
+        db_session.add(self)
+        db_session.commit()
 
     def add_feature_attempt(self, feature_list):
         """Adds a feature attempt with an incrementing index"""
-        attempt_index = len(self.feature_attempts) + 1
-        self.feature_attempts[attempt_index] = feature_list
+        self.feature_attempts.append(feature_list)
         db_session.add(self)
         db_session.commit()
 
@@ -49,9 +54,10 @@ def add_exercise(exercise_key, exercise_text, skel_code):
         exercise_text: The description / question
         skel_code: The skeleton code
     """
-    exercise = ExerciseEntry(exercise_key, exercise_text, skel_code)
-    db_session.add(exercise)
-    db_session.commit()
+    if not exercise_exists(exercise_key=exercise_key):
+        exercise = ExerciseEntry(exercise_key, exercise_text, skel_code)
+        db_session.add(exercise)
+        db_session.commit()
 
 
 def get_exercise(exercise_key):
@@ -60,6 +66,8 @@ def get_exercise(exercise_key):
         exercise_key=exercise_key).first()
     if exercise:
         return exercise
+
+    print("=== Exercise no found ===")
 
     return None
 
@@ -80,11 +88,27 @@ def get_feature_attempts(exercise_key, last_n=3):
     exercise = get_exercise(exercise_key=exercise_key)
 
     if exercise:
-        all_feature_attempts = exercise.feature_attempts.values()
-
-        return list(all_feature_attempts)[-last_n:]
+        return exercise.feature_attempts[-last_n:]
 
     return None
+
+
+def set_required_concepts(exercise_key: str, required_concepts: List[str]):
+    exercise = get_exercise(exercise_key=exercise_key)
+
+    if exercise:
+        exercise.set_required_concepts(required_concepts=required_concepts)
+    else:
+        print("\n=== Exercise doesn't exist ===\n")
+
+
+def get_required_concepts(exercise_key: str):
+    exercise = get_exercise(exercise_key=exercise_key)
+
+    if exercise:
+        return exercise.required_concepts
+    else:
+        print("\n=== Exercise doesn't exist ===\n")
 
 
 # Delete the database

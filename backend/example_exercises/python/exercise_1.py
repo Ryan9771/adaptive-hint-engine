97dae976307d914ccf0fde185d2414e2cc2b53de
Ryan <rkp21@ic.ac.uk>
2025-05-11 16:12:53 +0100

    REFACTOR: Modified values for struggling and improving threshold

--- backend/example_exercises/python/exercise_1.py
+++ backend/example_exercises/python/exercise_1.py
@@ -14,11 +14,11 @@ student_code = """
 def fizzbuzz(n):
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            print(i)
-        if i % 3 == 0:
-            print(i)
-        if i % 5 == 0:
-            print(i)
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
 """
 
 # student_code = """
